1ac2c7deeb7d2299bcd095a2eeb1c34e24f150bd
draguu <dragoneex@gmail.com>
2017-03-03 20:34:39 +0530

    v1.1

--- __init__.py
+++ __init__.py
@@ -22,7 +22,7 @@ bl_info = {
     "name": "Suzzane",
     "description": "",
     "author": "Pratik Solanki",
-    "version": (1,0),
+    "version": (1,1),
     "blender": (2, 78, 0),
     "location": "View3D",
     "warning": "This addon is still in development.",
